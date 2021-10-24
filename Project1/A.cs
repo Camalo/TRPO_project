86cac14b3808478ea21dcc61343ffbb57c75b198
camal <camalovalsu29@gmail.com>
2021-10-24 20:52:06 +0300

    add a second lab

--- Project1/A.cs
+++ Project1/A.cs
@@ -1,18 +1,38 @@
-﻿namespace Project1
+﻿using System;
+
+namespace Project1
 {
-    public class A : C
+    public class A 
     {
-        public A(object b1, object b2, object b3, object c) : base(b1)
+        //должен хранить любое количество корней (0, 1, 2)
+        private float[] x;
+
+        public float[] X { get => x; set => x = value; }
+
+        public float[] SolveEquation(float a, float b)
         {
-            this.b2 = b2;
-            this.b3 = b3;
-            this.c = c;
-            
+
+            if (a != 0 && b != 0)
+            {
+                X = new float[1];
+                X[0] = -(b / a);
+               
+            } else if (a != 0)
+            {
+                X = new float[1];
+                X[0] = 0;
+                return X;
+
+            } else {
+                //Уравнения не существует, поэтому Х = NaN
+
+                X = new float[1];
+                X[0] = (float)Double.NaN;
+            }
+
+            return X;
+
         }
 
-        public object b1;
-        public object b2;
-        public object b3;
-        public object c;
     }
 }
--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,16 +1,27 @@
-﻿namespace Project1
+﻿using System;
+
+namespace Project1
 {
 
     class Program
     {
         static void Main(string[] args)
         {
-            //Создается объект класса B, на него ссылается экземпляр класса A и  экземпляр класса C
-            B b1 = new B();
-            
-            A a = new A(b1, new B(), new B(), new C(b1));
-           // a        b1       b2      b3         c
-           
+          
+            B solution = new B();
+            //1, 0, -16
+            //float[] actual = 
+            float[] actual = solution.SolveEquation(0, 0, 0);
+            if (Double.IsNaN(actual[0])) Console.WriteLine("Решения нет");
+
+            Console.WriteLine($"Уравнение 1: факт {actual[0]}");
+            ////4, -6, 0
+            //actual = solution.SolveEquation(4, -6, 0);
+            //Console.WriteLine($"Уравнение 1: ожидалось 1.5, 0 ; факт {actual[0]},{actual[1]}");
+
+            ////1, -6, 9
+            //actual = solution.SolveEquation(1, -6, 9);
+            //Console.WriteLine($"Уравнение 1: ожидалось 3 ; факт {actual[0]}");
         }
     }
 }
